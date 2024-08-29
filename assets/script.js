bd87cf768102bb3bfcdea1f217cc7067fc8bd879
anupkankale <anupkankaleak47@gmail.com>
2024-08-29 23:14:16 +0530

    It's for gulpfile.js setup directory so the previous code will be safe on main branch

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,22 @@ window.addEventListener("load", function () {
 
   var tab = this.document.querySelectorAll("ul.nav-tab > li");
 
-  for (let i = 0; i < tab.length; i++) {
-    tab[i].addEventListener("click", switchTab);
-  }
+  for (let i = 0; i < tab.length; i++) 
+   {
+     tab[i].addEventListener("click", switchTab);
+    }
 
   function switchTab(event) {
     event.preventDefault();
-     document.querySelector("ul.nav-tab li.active").classList.remove("active");
-     document.querySelector(".tab-pane.active").classList.remove("active");
+    document.querySelector("ul.nav-tab li.active").classList.remove("active");
+    document.querySelector(".tab-pane.active").classList.remove("active");
 
-      var clickedTab = event.currentTarget;
-      var anchor = event.target;
-      var activePaneID = anchor.getAttribute("href");
-      clickedTab.classList.add("active");
-      document.querySelector(activePaneID).classList.add("active");
-      
+    var clickedTab = event.currentTarget;
+    var anchor = event.target;
+    var activePaneID = anchor.getAttribute("href");
+
+    clickedTab.classList.add("active");
+    document.querySelector(activePaneID).classList.add("active");
   }
-});
+}
+);
