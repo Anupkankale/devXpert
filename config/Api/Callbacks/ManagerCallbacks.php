07ed0a8087c25d528603657db9bbf80ff592ae6c
anupkankale <anupkankaleak47@gmail.com>
2024-08-31 21:51:15 +0530

    code update

--- config/Api/Callbacks/ManagerCallbacks.php
+++ config/Api/Callbacks/ManagerCallbacks.php
@@ -8,24 +8,23 @@ namespace Config\Api\Callbacks;
 use Config\Base\BaseController;
 
 class ManagerCallbacks extends BaseController
-{
-    
-   public function  checkboxSanitize($input)
-   {
-     //return filter_var($input, FILTER_SANITIZE_NUMBER_INT);
-     return( isset($input) ? true : false);
-   }
- 
-   public function adminSectionManager(){
-    echo 'Activate The Section And Feachure of this ';
-   }
-
-   public function checkboxField( $args )
-   {
-      $name= $args['label_for'];
-      $classes = $args['class'];   
+ {
+    public function checkboxSanitize( $input )
+    {
+      // return filter_var($input, FILTER_SANITIZE_NUMBER_INT);
+      return ( isset($input) ? true : false );
+    }
+  
+    public function adminSectionManager()
+    {
+      echo 'Manage the Sections and Features of this Plugin by activating the checkboxes from the following list.';
+    }
+  
+    public function checkboxField( $args )
+    {
+      $name = $args['label_for'];
+      $classes = $args['class'];
       $checkbox = get_option( $name );
-    echo '<input type="checkbox" name="'.$name.'" value="1" class="'.$classes .'"'.($checkbox ? 'checked':'').'>';
-   }
-
-}
+      echo '<input type="checkbox" name="' . $name . '" value="1" class="' . $classes . '" ' . ($checkbox ? 'checked' : '') . '>';
+    }
+  }
