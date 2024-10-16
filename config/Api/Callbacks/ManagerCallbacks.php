d2ceda56c82b80879b6528741bf34be28384dc3b
anupkankale <anupkankaleak47@gmail.com>
2024-10-16 15:29:50 +0530

    up

--- config/Api/Callbacks/ManagerCallbacks.php
+++ config/Api/Callbacks/ManagerCallbacks.php
@@ -4,29 +4,33 @@
  *   @package Plugin_Development
  */
 namespace Config\Api\Callbacks;
+
 use Config\Base\BaseController;
 
 class ManagerCallbacks extends BaseController
 {
-  public function checkboxSanitize($input)
-  {
-    // return filter_var($input, FILTER_SANITIZE_NUMBER_INT);
-    return (isset($input) ? true : false);
-    
-  }
+    public function checkboxSanitize($input)
+    {
+        return (isset($input) ? true : false);
+    }
 
-  public function adminSectionManager()
-  {
-    echo 'Manage the Sections and Features of this Plugin by activating the checkboxes from the following list.';
-  }
-   
-  public function checkboxField( $args )
-	{
-		$name = $args['label_for'];
-		$classes = $args['class'];
-		$checkbox = get_option( $name );
-		echo '<input type="checkbox" name="' . $name . '" value="1" class="' . $classes . '" ' . ($checkbox ? 'checked' : '') . '>';
-	}
-  
-  
+    public function adminSectionManager()
+    {
+        echo 'Manage the Sections and Features of this Plugin by activating the checkboxes from the following list.';
+    }
+
+    public function checkboxField($args)
+    {
+        // Ensure $args is an array before accessing its elements
+        if (!is_array($args)) {
+            return;  // Exit early if $args is not an array
+        }
+
+        // Extract necessary variables from $args array
+        $name = isset($args['label_for']) ? $args['label_for'] : '';
+        $classes = isset($args['class']) ? $args['class'] : '';
+        $checkbox = get_option($name);
+
+        echo '<input type="checkbox" name="' . $name . '" value="1" class="' . $classes . '" ' . ($checkbox ? 'checked' : '') . '>';
+    }
 }
