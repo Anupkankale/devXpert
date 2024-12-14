55cbe7a0e579bb1249b21727d42ca10750c11558
Anup Kankale <73975381+Anupkankale@users.noreply.github.com>
2024-12-14 21:12:37 +0530

    Update SettingLinks.php

--- config/Base/SettingLinks.php
+++ config/Base/SettingLinks.php
@@ -4,15 +4,11 @@
  */
 namespace Config\Base;
 
-use \Config\Base\BaseController;
+  use \Config\Base\BaseController;
   class SettingLinks extends BaseController
   {
- 
-
-   public  function register(){
+        public  function register(){
         add_filter("plugin_action_links_$this->plugin", array($this, 'setting_link'));
-      
-         
     }
 
     public function setting_link($links)
