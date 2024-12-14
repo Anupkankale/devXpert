029c79dad18cb350d10220681abd1e8036d72850
Anup Kankale <73975381+Anupkankale@users.noreply.github.com>
2024-12-14 21:13:33 +0530

    Update admin.php

--- template/admin.php
+++ template/admin.php
@@ -1,9 +1,7 @@
 <div class="wrap">
     <h1> DevXpert Plugin </h1>
     <?php settings_errors();  ?>
-
-
-    <ul class="nav-tab">
+        <ul class="nav-tab">
         <li class="active"><a href="#tab-1">Manage Settings</a></li>
         <li><a href="#tab-2"> Updates </a></li>
         <li><a href="#tab-3"> About </a></li>
