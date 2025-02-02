a19a61f87e9d04b2a957f0776645dcad6a27364d
Anup Kankale <73975381+Anupkankale@users.noreply.github.com>
2025-02-02 23:03:40 +0530

    Update Enqueue.php

--- config/Base/Enqueue.php
+++ config/Base/Enqueue.php
@@ -1,21 +1,20 @@
 <?php /**
  *   @package Plugin_Development
-  
  */
+
 namespace Config\Base;
 use \Config\Base\BaseController;
 
  class Enqueue extends BaseController
  {
-     public function register(){
-        add_action('admin_enqueue_scripts', array($this, 'enqueue'));
+    public function register(){
+    add_action('admin_enqueue_scripts', array($this, 'enqueue'));
     }
-
-    //This Function is use for link our CSS and JavaScript File
+   
+   //This Function is use for link our CSS and JavaScript File
     function enqueue()
     {
         wp_enqueue_style('mypluginstyle',   $this->plugin_url .'/assets/style.css');
         wp_enqueue_script('mypluginScript', $this->plugin_url .'/assets/script.js');
     }
-
-    }
+ }
