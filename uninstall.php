df30ea38000a7e28389cbf8da3c9281e47fd5b26
Anup Kankale <73975381+Anupkankale@users.noreply.github.com>
2025-02-03 22:46:45 +0530

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -2,26 +2,21 @@
  /**
   * Trigger this file on Plugin unsintall
   *   @package Plugin_Development
+  */
 
-*/
-
-if (! defined('WP_UNINSTALL_PLUGIN')) {
+  if (! defined('WP_UNINSTALL_PLUGIN')) {
      die;
- }
+   }
 
 // Clear Database Stored Data
-
 /*
 Method 1
 $books = get_post(array('post_type' =>'book','numberposts'=>-1));  
-
 foreach($books as $book){
     wp_delete_post($book->ID, true);
 } */
 
-
-
- //Access the database via SQL 
+//Access the database via SQL 
  //Method 2
 global $wpdb;
 $wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book' ");
